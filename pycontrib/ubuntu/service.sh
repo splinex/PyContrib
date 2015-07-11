ce604187393fde6f9b9c626a45b696023c279335
human <mkorolev@splinex.com>
2015-07-11 13:50:13 -0400

    service.sh

--- pycontrib/ubuntu/service.sh
+++ pycontrib/ubuntu/service.sh
@@ -2,11 +2,16 @@
 
 set +e
 
-source ~/.bashrc
+sleep 10
+
+source /home/human/.bashrc
+export PYTHONPATH=$PYTHONPATH:/home/human/PyContrib
+PYTHONPATH=$PYTHONPATH:/home/human/PyContrib
 
 while true
 do
 	screen -X -S $1 quit
+	sleep 5
 	screen -d -m -S proxy python3 $2 --config $3
 	inotifywait -e modify $3
 done
