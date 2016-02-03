c33b49751d97a9cb5599b602ea72b3616fa5bd46
Anton Soloninin <asoloninin@splinex.com>
2016-02-03 18:02:00 +0500

    Fix redirect after login

--- pycontrib/service/login.py
+++ pycontrib/service/login.py
@@ -26,10 +26,16 @@ class HttpLoginHandler(LoginRequestHandler):
                    '<center><br><h1>Authentication is required</h1>'
                    '<b>Login:</b> <input type="login" name="login"><br><br>'
                    '<b>Password:</b> <input type="password" name="password"><br><br>'
-                   '<input type="submit" value="Sign in"><center>'
-                   '</form></body></html>'.format(**self.settings))
+                   '<input type="submit" value="Sign in"></center>'.format(**self.settings))
+        prev_page = self.request.arguments.get('next')
+        if prev_page:
+            self.write('<input type="hidden" name="prev_page" value="{0}">'.format(prev_page[0].decode()))
+        self.write('</form></body></html>')
 
     def post(self):
         self.set_secure_cookie("user", self.get_argument("login"))
         self.set_secure_cookie("password", self.get_argument("password"))
-        self.redirect('./')
+        self.redirect(self.get_argument('prev_page', "./"))
+
+
+
