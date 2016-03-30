124211c54315971f5b1e7332ffc5856484300baa
maximkorolev <mkorolev@splinex.com>
2016-03-30 14:50:04 +0500

    singletone env

--- pycontrib/misc/patterns.py
+++ pycontrib/misc/patterns.py
@@ -3,14 +3,14 @@
 """Utile patterns."""
 
 
-class Singlton(object):
+class Singleton(object):
 
     _instance = None
     _inited = False
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singlton, cls).__new__(cls)
+            cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
     def __init__(self, *args, **kw):
--- pycontrib/service/environment.py
+++ pycontrib/service/environment.py
@@ -8,10 +8,39 @@ import argparse
 import configparser
 
 from pycontrib.misc.informer import Informer
+from pycontrib.misc.patterns import Singleton
 
+class Environment(Singleton):
+    '''
+    Environment configuration
+    '''
+    def initialize(self, config_file=None):
+        
+        if not config_file:
+            argparser = argparse.ArgumentParser()
+            argparser.add_argument("--config", help="configuration file - required")
+            argparser.add_argument("--port", help="binding port")
+            argparser.add_argument("--debug", help="debug mode")
+    
+            args = argparser.parse_args()
+    
+            if not args.config:
+                raise Exception('Use --help for args')
+            
+            config_file = args.config
+        else:
+            args = None
+            
+        config = configparser.ConfigParser()
+        config.optionxform = str
+        config.read(config_file)
 
-class _Environment:
-    def __init__(self, config):
+        if not args is None:
+            if args.port:
+                config['NETWORK']['port'] = args.port
+            if args.debug:
+                config['GENERAL']['debug'] = 'True'
+
         if 'LOGIN' in config:
             cl = config['LOGIN']
             login = {'admins': {cl['adminlogin']: cl['adminpass']}, 'users': {cl['userlogin']: cl['userpass']}}
@@ -23,7 +52,7 @@ class _Environment:
 
         self.login = login
         self.host = network.get('host', '127.0.0.1')
-        self.port = int(network.get('port', '0'))
+        self.port = int(network.get('port', '80'))
         self.name = general.get('name', 'service')
         self.debug = (general.get('debug', 'False') == 'True')
         self.home = general.get('home', '')
@@ -34,44 +63,3 @@ class _Environment:
         Informer.initEnv(self)
 
         Informer.info('{0} started at {1}:{2}'.format(self.name, self.host, self.port))
-
-    @classmethod
-    def from_file(cls, config_file):
-        if not config_file:
-            return None
-
-        config = cls._parse_config_ini(config_file)
-        return cls(config)
-
-    @classmethod
-    def from_args(cls):
-        argparser = argparse.ArgumentParser()
-        argparser.add_argument("--config", help="configuration file - required")
-        argparser.add_argument("--port", help="binding port")
-        argparser.add_argument("--debug", help="debug mode")
-
-        args = argparser.parse_args()
-
-        if not args.config:
-            raise Exception('Use --help for args')
-
-        config = cls._parse_config_ini(args.config)
-
-        if args.port:
-            config['NETWORK']['port'] = args.port
-        if args.debug:
-            config['GENERAL']['debug'] = 'True'
-
-        return cls(config)
-
-    @staticmethod
-    def _parse_config_ini(file_name):
-        config = configparser.ConfigParser()
-        config.optionxform = str
-        config.read(file_name)
-
-        return config
-
-
-def Environment(config_file=None):
-    return _Environment.from_file(config_file) or _Environment.from_args()
