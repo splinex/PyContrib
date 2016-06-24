e0a13d32814dca9024d545363127f3a1a3b98626
maximkorolev <mkorolev@splinex.com>
2016-06-24 18:14:25 +0500

    setup

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(name='pycontrib',
       version='1.0',
       install_requires=['psutil',
-                          'tornado>=4.3'],
+                        'tornado>=4.3',
+                        'motor>=0.6'],
       packages=['pycontrib',
                   'pycontrib.service',
                   'pycontrib.misc',
-                  'pycontrib.os'
+                  'pycontrib.os',
+                  'pycontrib.monguo'
                   ],
       )
